6647db533b9e6a3d8c62cf658caa0aa143e36b0e
Ricardo <ricardotavares16@gmail.com>
2018-12-05 16:30:35 +0000

    login inicio

---
+++ src/jsp/Person.java
@@ -0,0 +1,29 @@
+package jsp;
+
+import java.io.Serializable;
+
+/**
+ * Nesta class são guardadas as informações de cada utilizador.
+ */
+
+public class Person implements Serializable {
+    private boolean editor;
+    private String username;
+    private String password;
+
+    public Person(String username, String password, boolean editor){
+        this.username = username;
+        this.password = password;
+        this.editor = editor;
+    }
+
+    public String getUsername() { return this.username; }
+    public String getPassword() { return this.password; }
+    public boolean getEditor() { return this.editor; }
+    public void setEditor(Boolean editor) { this.editor = editor; }
+
+    @Override
+    public String toString(){
+        return this.username + "," + this.password + "," + this.editor;
+    }
+}
