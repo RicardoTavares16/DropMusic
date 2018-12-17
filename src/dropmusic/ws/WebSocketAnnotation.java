5ce43424f69b7ec4832ea74f737741c1b2d0dbe6
Ricardo <ricardotavares16@gmail.com>
2018-12-17 12:50:09 +0000

    Javadoc InterfaceRmi e Websockets

--- src/dropmusic/ws/WebSocketAnnotation.java
+++ src/dropmusic/ws/WebSocketAnnotation.java
@@ -8,6 +8,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Classe que gere os métodos das WebSockets
+ */
 @ServerEndpoint(value = "/ws")
 public class WebSocketAnnotation {
     private static final AtomicInteger sequence = new AtomicInteger(1);
@@ -20,6 +23,10 @@ public class WebSocketAnnotation {
         username = "User" + sequence.getAndIncrement();
     }
 
+    /**
+     * Ao iniciar, define a sessão e adiciona-a a uma lista de sessões
+     * @param session
+     */
     @OnOpen
     public void start(javax.websocket.Session session) {
         socketSessions.add(session);
@@ -28,12 +35,19 @@ public class WebSocketAnnotation {
         broadcast(message);
     }
 
+    /**
+     * Ao terminar a sessão, remove-a da lista
+     */
     @OnClose
     public void end() {
         // clean up once the WebSocket connection is closed
         socketSessions.remove(session);
     }
 
+    /**
+     * Ao haver uma mensagem, envia-a através da função sendMessage()
+     * @param message mensagem a ser enviada
+     */
     @OnMessage
     public void receiveMessage(String message) {
         // one should never trust the client, and sensitive HTML
@@ -48,6 +62,10 @@ public class WebSocketAnnotation {
         t.printStackTrace();
     }
 
+    /**
+     * Envia a mensagem pela WebSocket
+     * @param text mensagem a ser enviada
+     */
     private void sendMessage(String text) {
         try {
             this.session.getBasicRemote().sendText(text);
@@ -61,6 +79,10 @@ public class WebSocketAnnotation {
         }
     }
 
+    /**
+     * Envia a mensagem pela WebSocket a todos os utilizadores logados
+     * @param message mensagem a ser enviada
+     */
     public void broadcast(String message){
         for(Session session: socketSessions){
             try {
