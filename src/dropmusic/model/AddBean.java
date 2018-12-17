83635ca1096642e3b6c8a98605f6afd3d3266836
Ricardo <ricardotavares16@gmail.com>
2018-12-17 12:43:11 +0000

    Javadoc Auxfunc e Model

--- src/dropmusic/model/AddBean.java
+++ src/dropmusic/model/AddBean.java
@@ -10,6 +10,9 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
+/**
+ * Classe com os métodos de set e get auxiliares às funções de adição, estabelece ligação com o servidor RMI
+ */
 public class AddBean implements Serializable {
     public static final String SESSION_MAP_KEY = "addBean";
     private static final long serialVersionUID = 1L;
@@ -20,6 +23,11 @@ public class AddBean implements Serializable {
     private int id;
     private RmiInterface rmiServer;
 
+    /**
+     * Construtor
+     * Define o ID do cliente para as respostas multicast e estabelece a ligação com o servidor RMI
+     * @throws Exception
+     */
     public AddBean() throws Exception{
         try {
             id = (int) (Math.random() * 100);
@@ -30,14 +38,26 @@ public class AddBean implements Serializable {
         }
     }
 
+    /**
+     * Set da musica a adicionar
+     * @param musicToAdd
+     */
     public void setMusicToAdd(String musicToAdd) {
         this.musicToAdd = stringFilter.filter(musicToAdd);
     }
 
+    /**
+     * Set do album a adicionar
+     * @param albumToAdd
+     */
     public void setAlbumToAdd(String albumToAdd) {
         this.albumToAdd = stringFilter.filter(albumToAdd);
     }
 
+    /**
+     * Set do artista a adicionar
+     * @param artistToAdd
+     */
     public void setArtistToAdd(String artistToAdd) {
         this.artistToAdd = stringFilter.filter(artistToAdd);
     }
@@ -55,14 +75,30 @@ public class AddBean implements Serializable {
     }
 
     // Calls to RMI Server:
+
+    /**
+     * Função para adicionar musica, chama função correspondente do servidor RMI
+     * @return true/false dependendo da resposta do servidor
+     * @throws RemoteException
+     */
     public boolean addMusic() throws RemoteException {
         return rmiServer.addMusic(musicToAdd, id);
     }
 
+    /**
+     * Função para adicionar album, chama função correspondente do servidor RMI
+     * @return true/false dependendo da resposta do servidor
+     * @throws RemoteException
+     */
     public boolean addAlbum() throws RemoteException {
         return rmiServer.addAlbum(albumToAdd, id);
     }
 
+    /**
+     * Função para adicionar artista, chama função correspondente do servidor RMI
+     * @return true/false dependendo da resposta do servidor
+     * @throws RemoteException
+     */
     public boolean addArtist() throws RemoteException {
         return rmiServer.addArtist(artistToAdd, id);
     }
